3ca090566609de392ecc699ca934bdac96b72802
João Vieira <mutantinotrue@outlook.com>
2024-02-05 20:29:59 -0300

    first commit

---
+++ src/component/Banner/Banner.jsx
@@ -0,0 +1,9 @@
+import image from '../../assets/images/banner.png'
+
+function Banner() {
+    return (
+        <img src={"../../assets/images/banner.png"} alt="Banner principal" className={"banner"}/>
+    )
+}
+
+export default Banner
