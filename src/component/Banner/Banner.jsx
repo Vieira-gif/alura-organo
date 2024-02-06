328676311680ca54f9bd8e75b30b1113d7b655f8
João Vieira <mutantinotrue@outlook.com>
2024-02-06 11:43:50 -0300

    Ajustando a imagem

--- src/component/Banner/Banner.jsx
+++ src/component/Banner/Banner.jsx
@@ -2,7 +2,7 @@ import image from '../../assets/images/banner.png'
 
 function Banner() {
     return (
-        <img src={"../../assets/images/banner.png"} alt="Banner principal" className={"banner"}/>
+        <img src={image} alt="Banner principal" className={"banner"}/>
     )
 }
 
